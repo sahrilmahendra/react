977ad47efc31f20ad2f853d96199099a6cff395e
Moch Syahryil Mahendra <moch.mahendra@algo.co.id>
2024-03-10 14:06:18 +0700

    class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import './components/intro'
 import Intro from './components/intro';
+import Profile from './components/profile';
 
 function App() {
   return (
     <div>
-      <Intro name='Syahryil'/>
-      <Intro name='Mahendra'/>
+      <Intro name='Syahryil' lang='React JS'/>
+      <Intro name='Mahendra' lang='React JS'/>
+
+      <Profile/>
     </div>
   );
 }
--- src/components/intro.js
+++ src/components/intro.js
@@ -2,7 +2,7 @@ function Introduction(props) {
   return (
     <div>
       <h1>Introduction</h1>
-      <p>Hello World, I learn React JS</p>
+      <p>Hello World, I learn {props.lang}</p>
       <p>My Name {props.name}</p>
     </div>
   );
